da37bac22010bbdbb0ce988b56944461ab1a28e3
supriyabhor <supriyanaikwadi89@gmail.com>
2025-01-12 04:47:05 -0500

    add show gallery function

--- imageGallery.js
+++ imageGallery.js
@@ -1,3 +1,24 @@
+let currentPage = 1;
+const perPage = 10; // images
+
+export async function showGallery(category, page= currentPage, per_page = perPage)
+{
+    console.log(`Displaying gallery for category: ${category} (Page: ${page}, Per Page: ${per_page})`);
+
+    const images = await fetchImagesByCategory(category, page, per_page);
+    const galleryContainer = document.getElementById('gallery');
+    galleryContainer.innerHTML = '';  
+
+    images.forEach(image => {
+        const imgElement = document.createElement('img');
+        imgElement.src = image.src.small;
+        imgElement.alt = image.alt;
+        imgElement.dataset.id = image.id;
+        imgElement.addEventListener('click', showImageDetails);
+        galleryContainer.appendChild(imgElement);
+    });
+}
+
 
 
 export async function displayCategories(categories) 
@@ -10,7 +31,7 @@ export async function displayCategories(categories)
     li.textContent = category;
     li.addEventListener('click', ()=> {
         console.log('selected category is: ${category}');
-        displayGallery(category);
+        showGallery(category);
     })
     categoryList.appendChild(li);
    });
