6597e98be462544b067e9aa5d1a10471326239dd
supriyabhor <supriyanaikwadi89@gmail.com>
2025-01-12 04:18:57 -0500

    add function for popular category

--- api.js
+++ api.js
@@ -1,3 +1,13 @@
 const API_KEY = 'RjMK49lckHFPY6mHRQr3wGH2vIbrWlhB0nEYGNlUAfx9Im4IKWnUldSR'; 
 const image_URL = 'https://api.pexels.com/v1/';
 
+//add popular categories 
+export async function popularCatagory()
+{
+    console.log('Fetching categories...');
+    return ['Animal', 'Nature', 'Architecture', 'Food'];
+}
+
+//fetch images by category with pagination 
+// pagination- page and per page param
+
